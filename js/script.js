d0c3ccd29bab646cc77f56208c26c847c284b5ba
NFH-JHTM <vietnamneverdie1@gmail.com>
2025-04-13 09:54:57 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -42,6 +42,8 @@ let gameSolved = false;
 
 // 🎁 Click hộp quà
 giftBox.addEventListener("click", () => {
+  if (gameSolved) return; // Không thể nhấn khi đã hoàn thành
+
   popup.classList.remove("hidden");
   popup.classList.add("popup");
 
@@ -140,6 +142,9 @@ function handleCorrect() {
   yesBtn.style.position = "static";
   noBtn.style.position = "static";
   gameSolved = true;
+
+  // Gọi hiệu ứng hoa rơi từ petals.js
+  startPetalEffect();
 
   [yesBtn, noBtn].forEach(btn => {
     btn.onclick = () => {
